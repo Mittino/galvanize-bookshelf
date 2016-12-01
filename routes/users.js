fe75c3609d75147ff333dd763a239bf2f1cf0b91
Anna Mefford <annamefford@gmail.com>
2016-12-01 15:44:31 -0700

    completed part 3

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,32 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+var knex = require('../knex');
+const {decamelizeKeys, camelizeKeys} = require('humps');
+var bcrypt = require('bcrypt');
 
-router.post()
+router.post('/users', function(req, res, next){
+  var hashed = bcrypt.hashSync(req.body.password, 8);
+
+  knex('users')
+  .insert({
+    first_name: req.body.firstName,
+    last_name: req.body.lastName,
+    email: req.body.email,
+    hashed_password: hashed,
+  }, '*')
+  .then((user) => {
+    var newUser = camelizeKeys(user);
+    res.send({
+      id: newUser[0].id,
+      firstName: newUser[0].firstName,
+      lastName: newUser[0].lastName,
+      email: newUser[0].email
+    }).status(200);
+  })
+  .catch ((err) => {
+    next(err);
+  });
+});
 
 module.exports = router;
