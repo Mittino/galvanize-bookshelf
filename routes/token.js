d4d63dc255fce770dc4c3840a366bb23b06745fb
Anna Mefford <annamefford@gmail.com>
2016-12-02 10:25:24 -0700

    tokenPath reading file

--- routes/token.js
+++ routes/token.js
@@ -4,7 +4,26 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+var fs = require('fs');
+var path = require('path');
+var tokenPath = path.join(__dirname, '../.env');
+console.log(tokenPath);
 
-// YOUR CODE HERE
+
+var token = fs.readFileSync(tokenPath, 'utf8', function(err, data){
+  if (err) {
+    throw (err);
+  }
+});
+console.log(token);
+
+// router.get('/token', function(req, res, next){
+//   var decoded = jwt.decode(token, {complete: true});
+//   console.log(decoded.header);
+//   console.log(decoded.payload);
+//
+// });
+
 
 module.exports = router;
