fdb1b414b4375e002a55b82425c8893f3c97c68c
Anna Mefford <annamefford@gmail.com>
2016-12-02 11:00:51 -0700

    updating using dotenv

--- routes/token.js
+++ routes/token.js
@@ -5,25 +5,16 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-var fs = require('fs');
-var path = require('path');
-var tokenPath = path.join(__dirname, '../.env');
-console.log(tokenPath);
+require('dotenv').config();
 
+console.log(process.env.JWT_SECRET);
 
-var token = fs.readFileSync(tokenPath, 'utf8', function(err, data){
-  if (err) {
-    throw (err);
-  }
+router.get('/token', function(req, res, next){
+  var decoded = jwt.decode(token, {complete: true});
+  console.log(decoded.header);
+  console.log(decoded.payload);
+
 });
-console.log(token);
-
-// router.get('/token', function(req, res, next){
-//   var decoded = jwt.decode(token, {complete: true});
-//   console.log(decoded.header);
-//   console.log(decoded.payload);
-//
-// });
 
 
 module.exports = router;
